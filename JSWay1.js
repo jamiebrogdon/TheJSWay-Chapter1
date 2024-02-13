9b7b1eb00c0781b3a1f384e0ba6c94f312c9faf2
Jamie Brogdon <125220298+jamiebrogdon@users.noreply.github.com>
2024-02-13 13:14:38 -0600

    Add files via upload

---
+++ JSWay1.js
@@ -0,0 +1,17 @@
+//CHAPTER 1
+//Write a program that displays your name and age:
+//(decided on creating variables as opposed to the simpler 'console log' option in chapter 1 since I know how to do this)
+const myName = Jamie
+const age = 32
+console.log(myName + " " + age)
+
+
+//CHAPTER 1
+//Write a program that displays the results of adding, subtracting, multiplying and dividing 6 by 3
+//(decided on creating variables as opposed to the simpler 'console log' option in chapter 1 since I know how to do this)
+const num1 = 6
+const num2 = 3
+console.log(num1 + num2)
+console.log(num1 - num2)
+console.log(num1 / num2)
+console.log(num1 * num2)
